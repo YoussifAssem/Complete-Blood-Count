8eec5e02f2e8d3bcaf14ddd7d5e82f3305521ebc
MohamedMoataz266 <mohamedmoataz266266@gmail.com>
2021-11-01 00:47:56 +0200

    added home

---
+++ Complete Blood Count/Code/completebloodcount/lib/Screens/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:completebloodcount/Screens/signup.dart';
+import 'package:completebloodcount/widgets/login_form.dart';
+import 'package:completebloodcount/widgets/login_option.dart';
+
+
+class MyHomePage extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('CBC')),
+      body: ListView(
+        children: [
+          Center(child: Column(children: [Image.asset('images/home.jpg')],),)
+        ],
+      ),
+       
+      drawer: Drawer(child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(image: AssetImage('images/home.jpg'),
+              fit: BoxFit.fill,
+              ),
+              
+              //Center(child: Column(children: [SizedBox(height: 20), Image.asset('images/home.jpg')],),)
+            ),
+            child: Text('My account'),
+            
+            ),
+            ListTile(
+              title: const Text('Home Page'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Examine Self'),
+              onTap: () {},
+            ),
+
+        ],
+      ),
+      ),
+      
+    );
+  }
+
+}
