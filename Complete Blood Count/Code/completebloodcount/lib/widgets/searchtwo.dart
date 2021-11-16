9edc57550be4423e18929947f275ba94ff97a71d
kareem644 <kareem1802405@miuegypt.edu.eg>
2021-11-16 23:16:28 +0200

    search.dart
    search for a bloodcount

---
+++ Complete Blood Count/Code/completebloodcount/lib/widgets/searchtwo.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class search extends StatelessWidget {
+  const search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Search';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        ),
+      ],
+    );
+  }
+}
