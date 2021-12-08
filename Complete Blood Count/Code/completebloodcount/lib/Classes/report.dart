212215bf1d6239306a46c0da44f2da6dacb95ed1
MohamedMoataz266 <mohamedmoataz266266@gmail.com>
2021-12-08 21:19:09 +0200

    Created report.dart
    basic class report

---
+++ Complete Blood Count/Code/completebloodcount/lib/Classes/report.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+class Report{
+  String _problem = '';
+  String _suggest = '';
+  //Doctor doc = new Doctor();
+   final _firestore = FirebaseFirestore.instance;
+
+
+
+setProblem(String problem){
+  if (problem == '') {
+      //Display error in UI
+      throw 'Error, You have no problems listed';
+    }
+  _problem = problem;
+}
+
+
+
+setSuggest(String suggest){
+  if (suggest == '') {
+      //Display error in UI
+      throw 'Error, There are no suggestions here';
+    }
+  _suggest = suggest;
+}
+
+
+getProblem(){
+  return _problem;
+}
+
+
+
+getSuggest(){
+  return _suggest;
+}
+
+
+
+viewResults(){
+// this function is supposed to view the results of the report
+}
+
+
+suggestSolution(){
+// this function is supposed to suggest a solution based on the report shown to the user
+}
+
+
+}
+
+
+
