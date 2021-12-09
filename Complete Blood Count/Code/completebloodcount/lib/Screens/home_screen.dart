347a3c3d9a91da0c521f3eb119c5181a4ef85bbf
MohamedMoataz266 <mohamedmoataz266266@gmail.com>
2021-12-09 17:26:01 +0200

    Updated home screen
    rotation image

--- Complete Blood Count/Code/completebloodcount/lib/Screens/home_screen.dart
+++ Complete Blood Count/Code/completebloodcount/lib/Screens/home_screen.dart
@@ -3,19 +3,51 @@ import 'package:flutter/material.dart';
 import 'menu_screen.dart';
 
 // ignore: use_key_in_widget_constructors
-class MyHomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+
+
+
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: 7),
+    );
+
+    animationController.repeat();
+  }
+
+ 
+
   @override
   Widget build(BuildContext context) {
-    return Menu(
-      'Home Page',
-      ListView(
-        children: [
-          Center(
-            child: Column(
-              children: [Image.asset('images/dnap.jpg')],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(animationController),
+              child: Column(children: [Image.asset('images/dnap.jpg')],),
             ),
-          )
-        ],
+            
+           
+          ],
+        ),
       ),
     );
   }
