554f9f19c37b020894c9920cc467b4d2c0a9ebd7
MohamedMoataz266 <mohamedmoataz266266@gmail.com>
2021-12-09 15:31:39 +0200

    Updated searchtwo file
    Added in search bar and some miner adjustments

--- Complete Blood Count/Code/completebloodcount/lib/widgets/searchtwo.dart
+++ Complete Blood Count/Code/completebloodcount/lib/widgets/searchtwo.dart
@@ -1,24 +1,72 @@
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
-class search extends StatelessWidget {
+class search extends StatefulWidget {
   const search({Key? key}) : super(key: key);
+   
 
   @override
+  _SearchState createState() => _SearchState();
+
+}
+  class _SearchState extends State<search>{
+    //Icon customIcon = const Icon(Icons.search);
+    //Widget customSearchBar = const Text('Search');
+    bool isSearching = false;
+ @override
   Widget build(BuildContext context) {
+    
+    //Icon customIcon = const Icon(Icons.search);  //custom icon
     const appTitle = 'Search';
     return Scaffold(
       appBar: AppBar(
-        title: const Text(appTitle),
+        title: !isSearching ? const Text(appTitle)
+        :const TextField(
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            icon: Icon(Icons.search,
+            color: Colors.white),
+             hintText: 'Search Here...',
+             hintStyle: TextStyle(color: Colors.white)),
+             
+        ),
         backgroundColor: Colors.pink[900],
+        actions: <Widget> [
+          isSearching ?
+          IconButton(
+             icon: const Icon(Icons.cancel), 
+          
+            onPressed: () { 
+              setState(() {
+                this.isSearching = false;
+                });
+            },
+          ) : IconButton(
+             icon: const Icon(Icons.search), 
+          
+            onPressed: () { 
+              setState(() {
+                this.isSearching = true;
+                });
+            },
+          ),
+             
+               
+           
+           
+        ],
+        centerTitle: true,
       ),
       body: const MyCustomForm(),
     );
   }
+  
 }
 
 class MyCustomForm extends StatelessWidget {
+  
   const MyCustomForm({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
