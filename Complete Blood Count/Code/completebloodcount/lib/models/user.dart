45583060bbb7ca40a7bd79d013307476be787a09
YoussifAssem <youssifassem1815221@miuegypt.edu.eg>
2021-12-23 20:15:43 +0200

    Edit user

--- Complete Blood Count/Code/completebloodcount/lib/models/user.dart
+++ Complete Blood Count/Code/completebloodcount/lib/models/user.dart
@@ -6,47 +6,22 @@ import 'package:http/http.dart' as http;
 
 class User {
   // ignore: prefer_final_fields, unused_field
-  int _userID = 0;
-  String _firstname = '';
-  String _lastname = '';
-  String _birthdate = '';
+  String _name = '';
   String _email = '';
   String _phonenumber = '';
   String _password = '';
   String _gender = '';
   // ignore: prefer_final_fields
   String _type = '';
-  // ignore: unused_field
-  final _firestore = FirebaseFirestore.instance;
-  final url = Uri.https(
-      'complete-blood-count-default-rtdb.firebaseio.com', 'signup.json');
-  User() {
-    //_userID++;
-  }
-  createData() {
-    // ignore: unused_local_variable
-    Map<String, dynamic> users = {'email': _email};
-  }
 
-  //Named Constructor
-  User.addUser(String fN, String lN, String bD, String e, String pN, String p,
-      String g, String t) {
-    setFirstName(fN);
-    setLastName(lN);
-    setBirthDate(bD);
+  User.addUser(String n, String e, String pN, String p, String g, String t) {
+    setName(n);
     setEmail(e);
     setPhoneNumber(pN);
     setPassword(p);
     setGender(g);
     setType(t);
-    //_addIntoDataBase();
-    _addData();
-    _getData();
   }
-  getID() {
-    //Read ID From DataBase
-  }
-
   setType(String t) {
     if (t == 'Patient') {
       _type = t;
@@ -61,40 +36,18 @@ class User {
     return _type;
   }
 
-  setFirstName(String fN) {
-    if (fN == '') {
+  setName(String n) {
+    if (n == '') {
       //Display error in UI
       throw 'Error, please Enter Your First Name';
     }
-    _firstname = fN;
+    _name = n;
   }
 
-  getFirstName() {
-    return _firstname;
+  getName() {
+    return _name;
   }
 
-  setLastName(String lN) {
-    if (lN == '') {
-      throw 'error, please enter your last name';
-    }
-    _lastname = lN;
-  }
-
-  getLastName() {
-    return _lastname;
-  }
-
-  setBirthDate(String bD) {
-    if (bD == '') {
-      //Display error in UI
-      throw 'Error, please Enter Your Birth Date';
-    }
-    _birthdate = bD;
-  }
-
-  getBirthDate() {
-    return _birthdate;
-  }
 
   setEmail(String e) {
     if (e == '') {
@@ -150,7 +103,6 @@ class User {
   logIn(String email, String password) {
     setEmail(email);
     setPassword(password);
-    //Log In Join to the new page
   }
 
   sendMessage(String email, String message) {
@@ -158,23 +110,15 @@ class User {
   }
 
   editProfile(
-      {String? firstName,
-      String? lastName,
+      {String? name,
       String? email,
-      String? birthDate,
       String? phone,
       String? password}) {
-    setFirstName(firstName!);
-    setLastName(lastName!);
+    setName(name!);
     setEmail(email!);
     setPassword(password!);
-    setBirthDate(birthDate!);
     setPhoneNumber(phone!);
-
-    //This will edit the user profile and update the database
-    _updateDataInDataBase(getFirstName(), getLastName(), getEmail(),
-        getBirthDate(), getPhoneNumber(), getPassword());
-  }
+ }
 
   viewMessages() {
     //This function will view data from database
@@ -209,52 +153,12 @@ class User {
   _addData() async {
     Map<dynamic, dynamic> map = {
       // 'ID': _userID,
-      'firstName': getFirstName(),
-      'lastName': getLastName(),
+      'name': getName(),
       'email': getEmail(),
       'password': getPassword(),
-      'birthDate': getBirthDate(),
       'phoneNumber': getPhoneNumber(),
       'gender': getGender(),
       'type': getType(),
     };
-    // ignore: unused_local_variable
-
-    // ignore: unused_local_variable
-    await http.post(url, body: json.encode(map));
   }
-
-  // ignore: unused_element
-  _getData() async {
-    // ignore: unused_local_variable
-    final res = await http.get(url);
-    // ignore: unused_local_variable
-    final data = jsonDecode(res.body) as Map<dynamic, dynamic>;
-    data.forEach((key, value) {
-      // ignore: avoid_print
-      print(value);
-    });
-  }
-
-  // ignore: unused_element
-  _updateDataInDataBase(String firstName, String lastName, String email,
-      String birthDate, String phone, String password) async {
-    //This function will update data into data base
-    // ignore: unused_local_variable
-    Map<dynamic, dynamic> map = {
-      'firstName': firstName,
-      'lastName': lastName,
-      'email': email,
-      'password': password,
-      'birthDate': birthDate,
-      'phoneNumber': phone,
-    };
-    // ignore: unused_local_variable
-    final res = await http.get(url);
-    // ignore: unused_local_variable
-    final data = jsonDecode(res.body) as Map<dynamic, dynamic>;
-    data.forEach((key, value) async {
-      if (email == value) {}
-    });
-  }
-}
+ 
